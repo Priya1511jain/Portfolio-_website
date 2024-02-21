c76ae01189af812379e930015ee273d66e592947
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2024-02-22 00:19:29 +0530

    Add files via upload

---
+++ portfolio website/item.js
@@ -0,0 +1,21 @@
+var tabs = document.getElementsByClassName("tabs");
+var tabcontents = document.getElementsByClassName("tabcontents");
+
+function opentab(tabname){
+for(tab of tabs){
+   tab.classList.remove("active-link");
+}
+for(tabcontent of tabcontents){
+  tabcontent.classList.remove("active-tab");
+  }
+event.currentTarget.classList.add("active-link");
+document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
